ba931885a941c61442bdc273f1cb14c81ae3bfd9
FabioBenjamim <36937418+FabioBenjamim@users.noreply.github.com>
2020-03-14 23:37:59 -0300

    Arrumado
    arrumado

--- src/main/java/com/usjt/tcc/resource/ContasResource.java
+++ src/main/java/com/usjt/tcc/resource/ContasResource.java
@@ -1,26 +1,16 @@
 package com.usjt.tcc.resource;
 
-import java.util.List;
 import java.util.Optional;
 
-import javax.validation.Valid;
-
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.usjt.tcc.controller.LoginController;
 import com.usjt.tcc.model.Conta;
 import com.usjt.tcc.repository.Contas;
-import com.usjt.tcc.util.JPAUtil;
 
 @RestController
 @RequestMapping("/login")
