8329916c09dea56d93e7b6926ec5a8833642d2bf
unknown <ViniciusRibSantos>
2020-06-10 00:06:57 -0300

    Correção prever ação

--- src/main/java/com/usjt/tcc/repository/PredicaoRepository.java
+++ src/main/java/com/usjt/tcc/repository/PredicaoRepository.java
@@ -1,15 +1,20 @@
 package com.usjt.tcc.repository;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.usjt.tcc.model.entity.Acao;
 import com.usjt.tcc.model.entity.Predicao;
 
 @Repository
 public interface PredicaoRepository extends JpaRepository<Predicao, Long>{
 
-	Optional<Predicao> findFirstByInvestimentoIdAndData(long id, Date data);
+	@Query(nativeQuery = true, value = "select * from TB_ACAO_PREVISAO where data = :data and id_investimento = :id")
+	List<Predicao> findByIdData(@Param("id")long id, @Param("data")String data);
 }
