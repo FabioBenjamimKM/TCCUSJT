898b4c5ef7b662e2eb73c67f8aabbc93e847a5fe
FabioBenjamim <36937418+FabioBenjamim@users.noreply.github.com>
2020-09-03 18:15:33 -0300

    Oauth

--- src/main/java/com/usjt/tcc/AnaliseDeInvestimentoApplication.java
+++ src/main/java/com/usjt/tcc/AnaliseDeInvestimentoApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableResourceServer
 public class AnaliseDeInvestimentoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
