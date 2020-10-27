631f63505e5bca42b9bfa47292fd4c23daf6aefa
FabioBenjamim <47612412+FabioBenjamimKM@users.noreply.github.com>
2020-10-27 20:43:32 -0300

    update
    update

---
+++ auth/src/main/java/auth/controller/dto/UserDTO.java
@@ -0,0 +1,54 @@
+package auth.controller.dto;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import auth.entity.Role;
+
+public class UserDTO {
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	private List<String> roles;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public List<String> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+}
---
+++ auth/src/main/java/auth/entity/Role.java
@@ -0,0 +1,48 @@
+package auth.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Role implements GrantedAuthority {
+
+	public Role() {
+	}
+
+	public Role(String name) {
+		this.name = name;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+	private String name;
+
+	@Override
+	public String getAuthority() {
+		return this.name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
